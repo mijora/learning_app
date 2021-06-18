54c2044b4bbb8c6c82c7bec5db94b26a40bd508d
Ignas <aidzas@gmail.com>
2021-06-18 11:01:50 +0300

    lesson2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,13 @@
 class Address < ApplicationRecord
   belongs_to :country, required: false
 
+  after_create :send_email
+
   def full_address
     [street, post_code, country&.title].reject(&:blank?).join(', ')
+  end
+
+  def send_email
+    MainMailer.after_order_email(id).deliver_later
   end
 end
